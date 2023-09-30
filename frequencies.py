fc9fabbd066df5e8ebcb53375004fb2caf2e5cb9
Ashwina Kalanathan <ashwina.kalanathan@gmail.com>
2023-09-30 16:33:17 +0100

    solution has been coded in

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+#"""Frequencies function."""
+#"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    # Use a for loop to iterate through the items
+    for item in items:
+        # Convert each item into a string
+        item_str = str(item)
+
+        if item_str not in frequencies:
+            frequencies[item_str] = 1
+        else:
+            frequencies[item_str] += 1
+
     return frequencies
